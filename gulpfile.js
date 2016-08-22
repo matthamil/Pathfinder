43eb8f2d49108ec377e133aa871f32cc3c84c532
Matt Hamil <j.matthew.hamil@gmail.com>
2016-08-21 23:02:51 -0500

    Remove the now ignored directory node_modules

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+// Babel Task
+// Compiles ES6 to ES5
+gulp.task('babel', () => {
+  gulp.src('js/*.js')
+    .pipe(babel({
+        presets: ['es2015']
+      }))
+    .pipe(gulp.dest('es5'));
+});
+
+// Uglify Task
+// Minifies JavaScript files
+gulp.task('uglify', () => {
+  gulp.src('es5/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('minjs'));
+});
+
+// Default Task
+// Compiles to ES5 and minifies
+gulp.task('default', ['babel', 'uglify']);
